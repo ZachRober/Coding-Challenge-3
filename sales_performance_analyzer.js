4730b31af2d52270dc6627b4c05466a93ef2cbd2
ZachRober <nolethedgehogdie@gmail.com>
2024-09-08 21:22:44 -0400

    implement generatePerformanceReport function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -1,16 +1,15 @@
-const salesFigures = [5500,3000,2200,2600,6700];
-let length = salesFigures.length;
+const salesFigures = [5500,3000,2200,2600,6700];//define our sales data
 function calculateAverageSales (arr)
 {
-total = arr.reduce((x,y) => x+y,0);
-let Average = total/length;
+let total = arr.reduce((x,y) => x+y,0);//add sales data then average
+let Average = total/arr.length;
 return Average;
 }
 console.log(calculateAverageSales(salesFigures));
     
 function determinePerformanceRating(x)
     {
-let Avg = calculateAverageSales(salesFigures);
+let Avg = calculateAverageSales(x);
  if (Avg > 10000) {
     return "Excellent";//returning values if one is true
     }
@@ -32,8 +31,7 @@ console.log(rating);
     
     
 function findTopAndBottomPerformers(x){
-let newarr = x.map(data => data.sales);//make sales and names into seperate arrays
-let newarr2 = x.map(data => data.name);
+let newarr = x.map(data => data.sales.reduce((y,z)=> y + z,0));//make sales into a seperate array and total these (for future use)
 let min = Math.min(...newarr);//find min and max values of number array
 let max = Math.max(...newarr);
 let bottomPerformer = x[newarr.indexOf(min)];//find the corresponding value to min and max
@@ -41,22 +39,23 @@ let topPerformer = x[newarr.indexOf(max)];
 return {topPerformer, bottomPerformer};
 
     }
-let employees = [{name: "JIm", sales:4500}, {name: "Billy", sales: 8400}, {name: "Samantha",sales: 6700}];
+let employees = [{name: "JIm", sales:[4500]}, {name: "Billy", sales: [8400]}, {name: "Samantha",sales: [6700]}];
 console.log(findTopAndBottomPerformers(employees));
 
+
 function generatePerformanceReport(z){
-    av = ``
+    av = ``;
+     let tpbt = findTopAndBottomPerformers(z);
 for (let i = 0; i < z.length; i++){
-    let groupAverage = calculateAverageSales(z[i].sales);
-    av += `${groupAverage} `
-}
-tpbt = ``
-
-for (let i = 0; i <z.length; i++){
-    
-    sum = z.reduce((x[i], y[i]) => x[i].sales + y[i].sales, 0);
+    let groupAverage = calculateAverageSales(z[i].sales);//loop these functions for each line with in sales data
+    let prating = determinePerformanceRating(z[i].sales);
+    av += `${z[i].name}: Average: $${groupAverage}, Rating: ${prating}\n `
 
 }
+
+av+= `Top Performer: ${tpbt.topPerformer.name}, Bottom Performer: ${tpbt.bottomPerformer.name}`;
+return av;
+
 }
 const salesData = [
     { name: 'Alice', sales: [12000, 15000, 13000] },
@@ -64,3 +63,4 @@ const salesData = [
   { name: 'Charlie', sales: [3000, 4000, 3500] }, 
  { name: 'Diana', sales: [9000, 8500, 9200] },];
  console.log(generatePerformanceReport(salesData));
+ 
